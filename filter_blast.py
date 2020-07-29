e9394f0f852eda8698c0cd6b8fab8eacc150fe08
CassandriaTayFernandez <68932915+CassandriaTayFernandez@users.noreply.github.com>
2020-07-29 12:18:29 +0800

    Create filter_blast.py

---
+++ filter_blast.py
@@ -0,0 +1,19 @@
+#This script removes contaminants from the blast output based on the plant genus list. 
+#The All_plant_genus_list_updated.txt file can be replaced with any list of genuses
+###usage: python filter_blast.py [merged blast output] All_plant_genus_list_updated.txt > genome.best.hits.contaminantion.blast
+
+import sys
+f1=open(sys.argv[1],'r') 
+f2=open(sys.argv[2],'r') 
+genus=set()
+for line in f2:
+    genus.add(line.rstrip())
+for line in f1:
+    ll = line.split('\t')
+    thisgenus = ll[13].split()[0]
+    if 'PREDICTED' in thisgenus:
+        thisgenus = ll[13].split()[1]
+    if thisgenus in genus:
+       continue
+    else:
+       print line.replace('\n','')
